4e24ed79f37848c82ebe1d4c6b43d05ec3102526
HemanthTalari <144214464+HemanthTalari@users.noreply.github.com>
2025-08-08 11:26:53 +0530

    hemanth_navbar_fixed

--- sage-stack-ui/src/components/navbar/Navbar.jsx
+++ sage-stack-ui/src/components/navbar/Navbar.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
-import logo from "assets/images/logo.png"
+import React from "react";
+import logo from "assets/images/logo.png";
+import { Link } from "react-scroll";
 
 function Navbar() {
   return (
     <>
-<div className='w-[100%] flex justify-center fixed bottom-0'>
-   <div className=' flex justify-between p-4  items-center gap-8 bg-[#121212]/80 backdrop-blur-3xl rounded-xl ' >
-<img className='w-12' src={logo} />
+      <div className="w-[100%] flex justify-center fixed bottom-6">
+        <div className=" flex justify-between p-4  items-center gap-8 bg-[#121212]/80 backdrop-blur-3xl rounded-xl ">
+          <Link to="hero" smooth={true} duration={500} containerId="scroll-container">
+            <img className="w-12" src={logo} />
+          </Link>
+          <ul className="lg:flex md:flex hidden gap-4 items-center font-semibold shine-steel">
+            <Link to="about" smooth={true} duration={500} containerId="scroll-container">
+              <li className=" cursor-pointer">About</li>
+            </Link>
 
-<ul className='lg:flex md:flex hidden gap-4 items-center font-semibold shine-steel'>
-<li>About</li>
-<li>Services</li>
-<li>Contact Us</li>
-{/* <li className='text-white bg-[#343434] px-4 py-1 rounded-sm'>Contact Us</li> */}
-</ul>
-
+            <Link to="services" smooth={true} duration={500} containerId="scroll-container">
+              <li>Services</li>
+            </Link>
+            <Link to="contactUs" smooth={true} duration={500} containerId="scroll-container">
+              <li>Contact Us</li>
+            </Link>
+            {/* <li className='text-white bg-[#343434] px-4 py-1 rounded-sm'>Contact Us</li> */}
+          </ul>
+        </div>
       </div>
-
-   </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- sage-stack-ui/src/containers/home/Hero.js
+++ sage-stack-ui/src/containers/home/Hero.js
@@ -1,34 +1,18 @@
-import React, { useRef, useState, useEffect } from "react";
+import React from "react";
+import SplashCursor from "components/cursor/SplashCursor";
 
 function Hero() {
-  //  const [lastScroll, setLastScroll] = useState(0);
-  const [hidden, setHidden] = useState(false);
-
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const currentScroll = window.scrollY;
-
-  //     if (currentScroll > lastScroll) {
-  //       setHidden(true); // Scrolling down
-  //     } else {
-  //       setHidden(false); // Scrolling up
-  //     }
-
-  //     setLastScroll(currentScroll <= 0 ? 0 : currentScroll);
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-  //   return () => window.removeEventListener("scroll", handleScroll);
-  // }, [lastScroll]);
 
   return (
     <>
-      {/* <div id="scroll-header" className={`w-screen h-screen flex flex-col  justify-center items-center z-1000 animate-float top-10 left-1/2 -translate-x-1/2 transition-header ${
-        hidden ? "opacity-0 scale-50" : "opacity-100 scale-100"
-      }`}   > */}
+  
       <div
-        className=" h-screen flex flex-col justify-center items-center"
+        className=" h-[90vh] flex flex-col justify-center items-center"
       >
+
+            <SplashCursor />
+            
+
         <h1 className=" lg:text-8xl md:text-6xl font-[900] text-4xl  relative inline-block text-center shine-text">
           SAGE
           <br />
@@ -41,7 +25,6 @@ function Hero() {
           traffic, and intelligence.
         </h2>
       </div>
-      {/* </div> */}
     </>
   );
 }
